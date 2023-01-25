033946919251eb4af1af41b503bea3232ef98153
Marina Calheira de Oliveira <marina.oliveira@aln.senaicimatec.edu.br>
2023-01-25 11:33:08 -0300

    css

---
+++ curso-nextjs/exercicios/src/pages/css/integracao1.jsx
@@ -0,0 +1,23 @@
+import './integracao1.css'
+
+export default function integracao() {
+    return (
+        <>
+
+            <div className="vermelha">
+                Texto #1
+            </div>
+
+                
+            <div className="azul">
+                Texto #2
+            </div>
+            
+
+            <div className="branca">
+                Texto #3
+            </div>
+
+        </>
+    )
+}
