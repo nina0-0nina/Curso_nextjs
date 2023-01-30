f149a0b69e0a53f073ffdb57554126823d3a3ddc
Marina Calheira de Oliveira <marina.oliveira@aln.senaicimatec.edu.br>
2023-01-30 09:08:56 -0300

    integração com css #2

---
+++ curso-nextjs/exercicios/src/pages/css/integracao2.jsx
@@ -0,0 +1,24 @@
+// import
+import styles from "./integracao2.module.css"
+
+export default function integracao2 () {
+    return (
+        <div id = {styles.integracao2}>
+
+            <div className = {styles.vermelha}>
+                Texto #1
+            </div>
+
+                
+            <div className = {styles.azul}>
+                Texto #2
+            </div>
+            
+
+            <div className = {styles.branca}>
+                Texto #3
+            </div>
+
+        </div>
+    )
+}
