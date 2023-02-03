bec9bb585f723747c1c9611893102fba04dabc89
Marina Calheira de Oliveira <marina.oliveira@aln.senaicimatec.edu.br>
2023-02-03 10:13:41 -0300

    Chamada da função via evento

---
+++ curso-nextjs/exercicios/src/pages/basicos/botao.jsx
@@ -0,0 +1,35 @@
+function acao1(){
+    console.log("acao1")
+}
+
+export default function Botao(){
+    function acao2(){
+        console.log("acao2")
+    }
+
+    function acao5(e){
+        console.log(e)
+    }
+
+    return(
+        <div>
+            <button onClick={acao1}>Click #1</button>
+
+            <button onClick={acao2}>Click #2</button>
+
+            <button onClick={function(){
+                console.log('acao3')
+            }}>Click #3</button>
+
+            <button onClick={()=> console.log('acao4')}>Click #4</button>
+
+            <button onClick={acao5}>Click #5</button>
+
+            <button onClick={e => acao5(e.altKey)}>Click #6</button>
+
+            <div>
+               <input type="text" onChange={e => console.log(e.target.value)} /> 
+            </div>
+        </div>
+    )
+}
