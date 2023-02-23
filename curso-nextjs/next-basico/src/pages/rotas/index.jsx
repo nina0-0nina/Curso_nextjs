45403478ac67fa061eede715a7fb732f4898121f
Marina Calheira de Oliveira <marina.oliveira@aln.senaicimatec.edu.br>
2023-02-23 10:53:48 -0300

    Navegando entre páginas #2

--- curso-nextjs/next-basico/src/pages/rotas/index.jsx
+++ curso-nextjs/next-basico/src/pages/rotas/index.jsx
@@ -1,6 +1,23 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 export default function rotas(){
+    const router = useRouter()
+
+    function navegacaoSimples(url){
+        router.push(url)
+    }
+
+    function navegacaoComParams(){
+        router.push({
+            pathname: "/rotas/params",
+            query: {
+                id: 123,
+                nome: 'Anna'
+            }
+        })
+    }
+
     return(
         <div>
             <h1>Rotas Index</h1>
@@ -17,6 +34,13 @@ export default function rotas(){
                     <li>Maria</li>
                 </Link>
             </ul>
+
+            <div style={{display: "flex", flexDirection: "column", alignItems: "flex-start"}}>
+                <button onClick={() => router.push('/rotas/123/buscar')}>Buscar</button>
+                <button onClick={() => navegacaoSimples('/rotas/123/Maria')}>Maria</button>
+                <button onClick={navegacaoComParams}>Params</button>
+
+            </div>
         </div>
     )
 }
