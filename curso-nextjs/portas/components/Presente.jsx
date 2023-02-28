4fdcde21cfd5f4c6bcdf441e42ba15ea46a73ffe
Marina Calheira de Oliveira <calheiraoliveira@gmail.com>
2023-02-28 12:45:51 -0300

    Componente presente

--- curso-nextjs/portas/components/Presente.jsx
+++ curso-nextjs/portas/components/Presente.jsx
@@ -4,6 +4,11 @@ export default function Presente() {
     return(
         <div className={styles.presente}>
 
+            <div className={styles.tampa}></div>
+            <div className={styles.corpo}></div>
+            <div className={styles.laco1}></div>
+            <div className={styles.laco2}></div>
+
         </div>
     )
 }
