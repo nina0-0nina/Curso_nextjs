c4d2ab5267f114752bc87b0ba23e83825aac96f9
Marina Calheira de Oliveira <marina.oliveira@aln.senaicimatec.edu.br>
2023-03-01 08:58:52 -0300

    Componente porta #3

--- curso-nextjs/portas/components/Porta.jsx
+++ curso-nextjs/portas/components/Porta.jsx
@@ -2,8 +2,19 @@ import styles from "../src/styles/Porta.module.css"
 
 export default function Porta(props) {
     return(
-        <div className={styles.porta}>
-            <div className={styles.numero}>3</div>
+        <div className={styles.area}>
+
+            <div className={styles.estrutura}>
+
+                <div className={styles.porta}>
+
+                    <div className={styles.numero}>3</div>
+
+                </div>
+                
+            </div>
+            
         </div>
+        
     )
 }
