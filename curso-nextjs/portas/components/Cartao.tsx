cfc42a5911933504d986d6a5427e6da72f8bd727
Marina Calheira de Oliveira <marina.oliveira@aln.senaicimatec.edu.br>
2023-03-08 09:43:20 -0300

    comentando Cartao.tsx

--- curso-nextjs/portas/components/Cartao.tsx
+++ curso-nextjs/portas/components/Cartao.tsx
@@ -1,13 +1,20 @@
+// import
 import styles from '../src/styles/Cartao.module.css'
 
+// o interface é usado para definir tipos específicos, dessa forma, foi criado um objeto CartaoProps que possui 2 propriedades: bgcolor que é opcional(?)
+// e do tipo string e children que também é opcional e é do tipo any, ou seja, qualquer tipo de variável é aceita 
 interface CartaoProps {
     bgcolor?: string
     children?: any
 }
 
-export default function Cartao(props: CartaoProps){
-    return(
-        <div className = {styles.cartao}
+//a função é criada mandando o objeto criado anteriormente como parâmetro
+export default function Cartao(props: CartaoProps) {
+    return (
+        //criando uma div é estilizada pelo styles cartao
+        //e é adicionado um style condicional, onde o bgcolor é mandado pelo children e caso não seja mandado é usado a cor branca.
+        //backgroundcolor vai ser = a props.bgcolor, senão for enviado (??) é usado o '#fff'
+        <div className={styles.cartao}
             style={{
                 backgroundColor: props.bgcolor ?? '#fff'
             }}>
