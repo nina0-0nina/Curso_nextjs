f28940780878867c1cbf5ba18439d778b93e830c
Marina Calheira de Oliveira <marina.oliveira@aln.senaicimatec.edu.br>
2023-03-08 11:10:40 -0300

    Comentando porta.ts

--- curso-nextjs/portas/model/porta.ts
+++ curso-nextjs/portas/model/porta.ts
@@ -1,51 +1,56 @@
-export default class PortaModel{
+//criação da classe PortaModel
+export default class PortaModel {
     #numero: number
     #temPresente: boolean
     #selecionada: boolean
     #aberta: boolean
 
-    constructor(numero: number, temPresente = false, selecionada = false, aberta = false){
+    //contrutor para as variáveis
+    constructor(numero: number, temPresente = false, selecionada = false, aberta = false) {
         this.#numero = numero
         this.#temPresente = temPresente
         this.#selecionada = selecionada
         this.#aberta = aberta
     }
 
-    get numero(){
+    //criação dos métodos get, não necessário a criação do set
+    get numero() {
         return this.#numero
     }
 
-    
-    get temPresente(){
+
+    get temPresente() {
         return this.#temPresente
     }
 
-    get selecionada(){
+    get selecionada() {
         return this.#selecionada
     }
 
-    get aberta(){
+    get aberta() {
         return this.#aberta
     }
 
-    get fechada(){
+    get fechada() {
         return !this.aberta
     }
 
+    //a função desselecionar vai colocar o selecionada no false
     desselecionar() {
         const selecionada = false
         return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta)
     }
 
+    //a função alternar seleção vai modificar o this.selesionada para o contrário dela
     alternarSelecao() {
         const selecionada = !this.selecionada
         return new PortaModel(this.numero, this.temPresente, selecionada, this.aberta)
     }
 
+    //a função abrir vai mudar o aberta do false para o true
     abrir() {
         const aberta = true
         return new PortaModel(this.numero, this.temPresente, this.selecionada, aberta)
     }
-} 
+}
 
-    
